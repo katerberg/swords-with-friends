d513ce2952eb05cd58deb026d0206ec5be22c1d0
Mark Katerberg <katerberg@fastmail.fm>
2023-03-04 08:04:22 -0600

    client side setup

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import './index.scss';
+
+screen.orientation?.lock?.('portrait');
+
+function initCanvasSize(canvas: HTMLCanvasElement): void {
+  const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  canvas.width = 1000; //horizontal resolution (?) - increase for better looking text
+  canvas.height = 1778; //vertical resolution (?) - increase for better looking text
+  canvas.style.width = `${width}`; //actual width of canvas
+  canvas.style.height = `${height}`; //actual height of canvas
+  globalThis.gameElement = canvas;
+}
+
+window.addEventListener('load', () => {
+  const gameElement = document.getElementById('game-canvas') as HTMLCanvasElement;
+  if (gameElement) {
+    gameElement.onwheel = (event): void => {
+      event.preventDefault();
+    };
+
+    initCanvasSize(gameElement);
+  }
+});
