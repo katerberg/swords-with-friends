e7e529b6a0ee5c4112bc708e9310c2eb44b0af79
Mark Katerberg <katerberg@fastmail.fm>
2023-03-10 13:46:06 -0600

    Working with remote API

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 import {getServerPort} from './debug';
 
-// const SERVER_BASE = 'http://138.197.107.187:9081';
+// const SERVER_BASE = 'http://api.swordswithfriends.org:443';
 // const SERVER_BASE = `http://192.168.50.132:${getServerPort()}`;
 const SERVER_BASE = `http://localhost:${getServerPort()}`;
 export const SOCKET_BASE = SERVER_BASE;
