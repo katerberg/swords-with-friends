d24f167771fb74159ec5bcc8ec31473cd8d5a2d5
Mark Katerberg <katerberg@fastmail.fm>
2023-03-10 15:04:19 -0600

    linting

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import {getServerPort} from './debug';
 
 // const SERVER_BASE = ;
 // const SERVER_BASE = `http://192.168.50.132:${getServerPort()}`;
-const SERVER_BASE =
-  process.env.NODE_ENV === 'production' ? 'https://api.swordswithfriends.org:443' : `http://localhost:${getServerPort()}`;
+const isProd = process.env.NODE_ENV === 'production';
+const SERVER_BASE = isProd ? 'https://api.swordswithfriends.org:443' : `http://localhost:${getServerPort()}`;
 export const SOCKET_BASE = SERVER_BASE;
 export const API_BASE = `${SERVER_BASE}/api`;
