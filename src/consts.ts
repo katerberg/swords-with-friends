2441591ba2bbfb53379653f23fb2c06454accb19
Mark Katerberg <katerberg@fastmail.fm>
2023-03-10 08:57:13 -0600

    Allows stable build

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,7 @@
 import {getServerPort} from './debug';
 
-const SERVER_BASE = `http://192.168.50.132:${getServerPort()}`;
+const SERVER_BASE = 'http://138.197.107.187:9081';
+// const SERVER_BASE = `http://192.168.50.132:${getServerPort()}`;
 // const SERVER_BASE = 'http://localhost:8081';
 export const SOCKET_BASE = SERVER_BASE;
 export const API_BASE = `${SERVER_BASE}/api`;
